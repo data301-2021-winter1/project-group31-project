887cf4a0b2657362ea9870bc51627c18831c0f74
Jeremy Scarcella <jeremy.scarcella@alumni.ubc.ca>
2021-10-29 10:35:41 -0700

    added project functions

---
+++ script/project_functions1.py
@@ -0,0 +1,27 @@
+def load_and_process():
+
+
+        df1 = (
+                pd.read_csv("/Users/JeremyScarcella/project-group31-project/player_info.csv")
+                .drop('height', axis=1)
+                .drop('birthCity', axis=1)
+                .drop('birthDate', axis=1)
+                .drop('birthStateProvince', axis=1)
+        )
+        
+        
+        
+        df2 = (
+                df1
+                .rename(columns={"player_id": "Player ID"})
+                .set_index('primaryPosition')                                       
+                .rename(index={"D": "Defence"})
+                .rename(index={"G": "Goalie"})
+                .rename(index={"C": "Center"})
+                .rename(index={"RW": "Right Wing"})
+                .rename(index={"LW": "Left Wing"})
+        )
+        
+        return df2
+    
+    
